4525ff14e2d1badb4865c14bc00c4fc668949635
Guilherme-07062002 <guilherme762002@gmail.com>
2024-10-05 14:55:40 -0300

    feat: add svelte project files

---
+++ index.php
@@ -0,0 +1,23 @@
+<script>
+const svelte_script = document.createElement('script');
+svelte_script.type = 'text/javascript';
+svelte_script.defer = true;
+svelte_script.src = './svelte/public/build/bundle.js';
+document.head.appendChild(svelte_script);
+
+const svelte_global_stylesheet = document.createElement('link');
+svelte_global_stylesheet.rel = 'stylesheet';
+svelte_global_stylesheet.href = './svelte/public/global.css';
+document.head.appendChild(svelte_global_stylesheet);
+
+const svelte_compiled_stylesheet = document.createElement('link');
+svelte_compiled_stylesheet.rel = 'stylesheet';
+svelte_compiled_stylesheet.href = './svelte/public/build/bundle.css';
+document.head.appendChild(svelte_compiled_stylesheet);
+</script>
+
+<?php
+echo "Olá, Mundo!";
+?>
+
+<div id="svelte-app"></div>
